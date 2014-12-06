822c0eea9fc3524c2e1e75638a301b67b560359a
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2014-12-06 17:07:07 -0200

    Now using getpwuid

--- user_info.c
+++ user_info.c
@@ -17,5 +17,18 @@ int main()
 	printf("\tLogin shell: %s\n", pwd->pw_shell);
 	printf("\tGroup id: %d\n", pwd->pw_gid);
 
+	uid_t uid = pwd->pw_uid;
+
+	pwd = getpwuid(uid);
+	if (!pwd) {
+		printf("User not found! Aborting\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("User marcos(using getpwuid):\n");
+	printf("\tHome dir: %s\n", pwd->pw_dir);
+	printf("\tLogin shell: %s\n", pwd->pw_shell);
+	printf("\tGroup id: %d\n", pwd->pw_gid);
+
 	return EXIT_SUCCESS;
 }
